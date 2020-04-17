45e3297f99caca001014379c1325f39632825897
Javier Tibi <javiert@ornsoft.com>
2020-04-17 18:28:51 -0400

    Send Message

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Telegram\Bot\Api;
 use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Laravel\TelegramServiceProvider;
 
 class BotController extends Controller
 {
     public function handle()
     {
-        $updates = Telegram::getWebhookUpdates();
+        $response = Telegram::getWebhookUpdates();
+
+        $response = Telegram::sendMessage([
+            'chat_id' => $response[0]['message']['chat']['id'],
+            'text' => 'Hello World'
+        ]);
+
+        $messageId = $response->getMessageId();
+
         return 'ok';
     }
 }
