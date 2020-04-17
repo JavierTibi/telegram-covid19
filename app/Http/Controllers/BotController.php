4ec3ae936e71103f3960a4a9291d86997d4d2d3b
Javier Tibi <javiert@ornsoft.com>
2020-04-17 16:38:05 -0400

    BotController

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function getUpdate(Request $request)
+    {
+        return "Hola";
+    }
+}
