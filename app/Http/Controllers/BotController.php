2519042c703b027d7a157b28aed949511d80ee62
Javier Tibi <javiert@ornsoft.com>
2020-04-17 23:35:39 -0400

    Fix api connection

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,26 +10,29 @@ class BotController extends Controller
     public function handle()
     {
         $response = Telegram::getWebhookUpdates();
+        $client = new GuzzleHttp\Client();
 
+        //Get Country
         $country = $response['message']['text'];
+        try {
+            //API Countries
+            $res = $client->get("https://restcountries.eu/rest/v2/name/" . $country);
 
-        $responseCountry = file_get_contents("https://restcountries.eu/rest/v2/name/" . $country);
-        $isoCountry = json_decode($responseCountry)[0]->alpha2Code;
+            $responseCountry = $res->getBody()->getContents();
+            $isoCountry = json_decode($responseCountry)[0]->alpha2Code;
 
-
-        if ($isoCountry) {
-            $client = new GuzzleHttp\Client();
-            $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=AR&onlyCountries=true");
+            //API Coronavirus
+            $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=$isoCountry&onlyCountries=true");
             $content = $res->getBody()->getContents();
             Telegram::sendMessage([
                 'chat_id' => $response['message']['chat']['id'],
-                'text' => 'Datos de ' . json_decode($responseCountry)[0]->name . 'hasta el momento: \n' .
-                           'Casos confirmados: ' . json_decode($content)[0]->confirmed . '\n' .
-                           'Fallecidos: ' . json_decode($content)[0]->deaths . '\n'.
-                           'Recuperados: ' . json_decode($content)[0]->recovered . '\n'.
-                            '¡Quedate en casa!'
+                'text' => 'Datos de ' . json_decode($responseCountry)[0]->name . ' hasta el momento: ' .
+                    'Casos confirmados: ' . json_decode($content)[0]->confirmed . '.' .
+                    'Fallecidos: ' . json_decode($content)[0]->deaths . '.'.
+                    'Recuperados: ' . json_decode($content)[0]->recovered . '.  '.
+                    '¡Quedate en casa!'
             ]);
-        } else {
+        } catch (\Exception $exception) {
             Telegram::sendMessage([
                 'chat_id' => $response['message']['chat']['id'],
                 'text' => 'Lo sentimos no hemos encontrado ningun país con ese nombre. Intente nuevamente.'
