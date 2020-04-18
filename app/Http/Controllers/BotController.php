a232e4de5c67b1e24ec2266b21ac9a71275aa912
Javier Tibi <javiert@ornsoft.com>
2020-04-18 13:23:13 -0400

    Add last day and clean code

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,37 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Collection;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use GuzzleHttp;
 
 class BotController extends Controller
 {
+    /**
+     * Telegram Message Handler
+     */
     public function handle()
     {
         $response = Telegram::getWebhookUpdates();
-        $client = new GuzzleHttp\Client();
 
         //Get Country
-        $country = $response['message']['text'];
+        $country = $response[0]['message']['text'];
+
         try {
-            //API Countries
-            $res = $client->get("https://restcountries.eu/rest/v2/name/" . $country);
+            $countries = $this->getCountries($country);
+            foreach ($countries as $country) {
 
-            $responseCountries = $res->getBody()->getContents();
-            foreach (json_decode($responseCountries) as $respCountry) {
+                $infoByCountry = $this->getInfoByIsoCountry($country->alpha2Code);
 
-                $isoCountry = $respCountry->alpha2Code;
-
-                //API Coronavirus
-                $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=$isoCountry&onlyCountries=true");
-                $content = $res->getBody()->getContents();
+                $lastDay = $this->calculeLastDay($country->alpha2Code);
 
                 Telegram::sendMessage([
                     'chat_id' => $response['message']['chat']['id'],
-                    'text' => 'Datos de ' . $respCountry->name . ' hasta el momento:  ' . PHP_EOL .
-                        'Casos confirmados: ' . json_decode($content)[0]->confirmed  .  PHP_EOL .
-                        'Fallecidos: ' . json_decode($content)[0]->deaths . PHP_EOL .
-                        'Recuperados: ' . json_decode($content)[0]->recovered . PHP_EOL . PHP_EOL .
+                    'text' => 'Datos de ' . $country->name . ' hasta el momento:  ' . PHP_EOL .
+                        'Casos confirmados: ' . $infoByCountry[0]->confirmed  .  PHP_EOL .
+                        'Fallecidos: ' . $infoByCountry[0]->deaths . PHP_EOL .
+                        'Recuperados: ' . $infoByCountry[0]->recovered . PHP_EOL . PHP_EOL .
+                        'Datos del último día: ' . PHP_EOL .
+                        'Casos confirmados: ' . $lastDay['confirmed']  .  PHP_EOL .
+                        'Fallecidos: ' . $lastDay['deaths'] . PHP_EOL .
+                        'Recuperados: ' . $lastDay['recovered'] . PHP_EOL . PHP_EOL .
                         '¡Quedate en casa!'
                 ]);
             }
@@ -43,5 +46,69 @@ class BotController extends Controller
                 'text' => 'Lo sentimos no hemos encontrado ningun país con ese nombre. Intente nuevamente.'
             ]);
         }
+    }
+
+    /**
+     * Get the country codes through an API "Rest Countries"
+     * @param string $country
+     * @return mixed
+     */
+    private function getCountries($country)
+    {
+        $client = new GuzzleHttp\Client();
+        $res = $client->get("https://restcountries.eu/rest/v2/name/" . $country);
+
+        return json_decode($res->getBody()->getContents());
+    }
+
+    /**
+     * Gets Coronavirus information by country through an API
+     * @param $isoCountry
+     * @return mixed
+     */
+    private function getInfoByIsoCountry($isoCountry)
+    {
+        $client = new GuzzleHttp\Client();
+        $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=$isoCountry&onlyCountries=true");
+        return json_decode($res->getBody()->getContents());
+    }
+
+    /**
+     * Gets Coronavirus time series by country through an API
+     * @param $isoCountry
+     * @return mixed
+     */
+    private function getTimeSeriesByIsoCountry($isoCountry)
+    {
+        $client = new GuzzleHttp\Client();
+        $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/timeseries?iso2=$isoCountry&onlyCountries=true");
+        return json_decode($res->getBody()->getContents());
+    }
+
+    /**
+     * Calculates the information for the last available day with a time series
+     * @param $isoCountry
+     * @return array
+     */
+    private function calculeLastDay($isoCountry)
+    {
+        $timeSerie = $this->getTimeSeriesByIsoCountry($isoCountry);
+        $col = new Collection();
+        foreach ($timeSerie[0]->timeseries as  $value) {
+            $col->push($value);
+        }
+        $tot = $col->count();
+
+        $confirmedLastDay = $col->nth($tot-1)[1]->confirmed - $col->nth($tot-2)[1]->confirmed;
+        $deathsLastDay = $col->nth($tot-1)[1]->deaths - $col->nth($tot-2)[1]->deaths;
+        $recoveredLastDay = $col->nth($tot-1)[1]->recovered - $col->nth($tot-2)[1]->recovered;
+
+        $result = [
+            'confirmedLastDay' => $confirmedLastDay,
+            'deathsLastDay' => $deathsLastDay,
+            'recoveredLastDay' => $recoveredLastDay
+        ];
+
+        return $result;
     }
 }
