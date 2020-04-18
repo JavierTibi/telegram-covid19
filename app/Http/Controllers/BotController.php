19167b46ec4c5c7bbe473dbbc9d73848c17fbdc8
Javier Tibi <javiert@ornsoft.com>
2020-04-18 00:13:10 -0400

    Fix several countries

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -13,33 +13,33 @@ class BotController extends Controller
         $client = new GuzzleHttp\Client();
 
         //Get Country
-        $country = $response['message']['text'];
+        $country = $response[0]['message']['text'];
         try {
             //API Countries
             $res = $client->get("https://restcountries.eu/rest/v2/name/" . $country);
 
             $responseCountries = $res->getBody()->getContents();
-            $i = 0;
             foreach (json_decode($responseCountries) as $respCountry) {
+
                 $isoCountry = $respCountry->alpha2Code;
 
                 //API Coronavirus
                 $res = $client->get("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?iso2=$isoCountry&onlyCountries=true");
                 $content = $res->getBody()->getContents();
+
                 Telegram::sendMessage([
                     'chat_id' => $response['message']['chat']['id'],
-                    'text' => 'Datos de ' . json_decode($respCountry)[$i]->name . ' hasta el momento:  ' .
+                    'text' => 'Datos de ' . $respCountry->name . ' hasta el momento:  ' .
                         'Casos confirmados: ' . json_decode($content)[0]->confirmed . '. ' .
                         'Fallecidos: ' . json_decode($content)[0]->deaths . '. '.
                         'Recuperados: ' . json_decode($content)[0]->recovered . '.  '.
                         '¡Quedate en casa!'
                 ]);
-                $i++;
             }
 
         } catch (\Exception $exception) {
             Telegram::sendMessage([
-                'chat_id' => $response['message']['chat']['id'],
+                'chat_id' => $response[0]['message']['chat']['id'],
                 'text' => 'Lo sentimos no hemos encontrado ningun país con ese nombre. Intente nuevamente.'
             ]);
         }
